e94d710b9180b8a81a14c21a3e9a10d0e8b4c2e8
mohnish-srikanth <mohnishsrikanth@gmail.com>
2026-01-18 18:18:46 +0530

    front end dev started

--- backend/app/services/sportsdb.py
+++ backend/app/services/sportsdb.py
@@ -3,7 +3,7 @@ import requests
 BASE_URL = "https://www.thesportsdb.com/api/v1/json"
 
 def get_pl_standings(api_key = "123"):
-    url = f"{BASE_URL}/{api_key}/lookuptable.php?l=4328&s=2024-2025"
+    url = f"{BASE_URL}/{api_key}/lookuptable.php?l=4328&s=2025-2026"
     return requests.get(url).json()
 
 def get_pl_fixtures(api_key = "123"):
